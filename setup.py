1a7e27c1af6f02810e3fed704b9f543f7106031b
Kenneth Marshall <kenny.mrshll@gmail.com>
2024-02-06 14:30:04 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name="pdfgetx3GUI",  # Required
+
+    version="1.0",  # Required
+
+    description="a GUI for running PDFgetX3",  # Optional
+    scripts=['pdfgetx3_gui.py'],
+    package_dir={'.':'pdfgetx3_gui'},
+    entry_points = {'console_scripts': ['pdfgetx3gui = pdfgetx3_gui:main',]},
+    #url="https://github.com/pypa/sampleproject",  # Optional
+    author="K. P. Marshall",  # Optional
+    # This should be a valid email address corresponding to the author listed
+    # above.
+    author_email="kenneth.marshall@esrf.fr",  # Optional
+
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+
+    packages= find_packages(),  # Required
+    python_requires=">=3.7, <3.10", #I believe these are the requirements for diffpy.pdfgetx
+)
